46996d60a5e6fc21f10a735d01f9c375687eb5d3
Irena <irena.cerniavskyte@gmail.com>
2019-09-09 01:23:08 +0300

    beginning of assignment

--- TowerOfHanoi/Program.cs
+++ TowerOfHanoi/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,44 @@ namespace TowerOfHanoi
     {
         static void Main(string[] args)
         {
+            Move('A', 'B', 'C', 3);
+            Console.Read();
         }
+        public static void Move(char x, char y, char z, int n, List<String> xStatus = null, List<String> yStatus = null, List<String> zStatus = null, int id = 1)
+        {
+            if (n > 0)
+            {
+                xStatus = xStatus ?? Enumerable.Range(1, n).Select(num => n - num + 1).ToList();
+                xStatus = xStatus ?? Enumerable.Range(1, n).Reverse().Select(num => num.ToString()).ToList();
+                yStatus = yStatus ?? new List<String>();
+                zStatus = zStatus ?? new List<String>();
+
+                Move(x, z, y, n-1, xStatus, zStatus, yStatus);
+
+                Console.Write("Diską {0} nuo {1} perkelti ant {2}. ", n, x, z);
+
+                zStatus.Add(xStatus[xStatus.Count - 1]);
+                xStatus.RemoveAt(xStatus.Count - 1);
+
+                Console.Write("A=(");
+                foreach (int i in xStatus) {
+                    Console.Write(i + ",");
+                }
+                Console.Write("), B=(");
+                foreach (int i in yStatus)
+                {
+                    Console.Write(i + ",");
+                }
+                Console.Write("), C=(");
+                foreach (int i in zStatus)
+                {
+                    Console.Write(i + ",");
+                }
+
+                Console.WriteLine(").");
+                Move(y, x, z, n-1, yStatus, xStatus, zStatus);
+            }
+        }
+
     }
 }
