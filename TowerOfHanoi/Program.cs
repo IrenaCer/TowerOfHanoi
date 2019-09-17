1521c8690d58169b00859632a4e0868c48640d96
Irena <irena@Gytiss-MacBook-Air.local>
2019-09-18 00:17:38 +0300

    added input, fixed output formatting

--- TowerOfHanoi/Program.cs
+++ TowerOfHanoi/Program.cs
@@ -11,20 +11,24 @@ namespace TowerOfHanoi
     {
         static void Main(string[] args)
         {
-            hb('A', 'B', 'C', 3);
+            Console.Write("Įveskite diskų skaičių: ");
+            string c = Console.ReadLine();
+            int value;
+            if (int.TryParse(c, out value) && value >= 1 && value <= 10) {
+                hb('A', 'B', 'C', value);
+            } else
+            {
+                Console.WriteLine("Įvedėte neteisingą diskų skaičių. Diskų skaičius gali būti nuo 1 iki 10.");
+            }
 
-            Console.Read();
         }
 
         public static void hb(char xx, char yy, char zz, int nn)
         {
             Status status = new Status(xx, yy, zz, nn);
 
-            //Move(x, y, z, n);
-            //}
-
-            Console.WriteLine("Veiksmu seka, kai n={0}", nn);
-            Console.WriteLine("Pradine busena A:({0}) B:({1}) C:({2})", string.Join(", ", status.getStatus()['A']), string.Join(", ", status.getStatus()['B']), string.Join(", ", status.getStatus()['C']));
+            Console.WriteLine("Veiksmų seka, kai n={0}", nn);
+            Console.WriteLine("Pradinė būsena A:({0}) B:({1}) C:({2}).", string.Join(", ", status.getStatus()['A']), string.Join(", ", status.getStatus()['B']), string.Join(", ", status.getStatus()['C']));
 
 
             void Move(char x, char y, char z, int n)
@@ -35,33 +39,23 @@ namespace TowerOfHanoi
 
                     status.count += 1;
 
-                    Console.Write("{3}. Diska {0} nuo {1} perkelti ant {2}. ", n, x, z, status.count);
+                    //Console.Write("{3}. Diska {0} nuo {1} perkelti ant {2}. ", n, x, z, status.count);
+                    Console.Write("{3,5}. Diską {0,2} nuo {1} perkelti ant {2}.         ", n, x, z, status.count);
 
-                    
+
                     status.getStatus()[z].Add(status.getStatus()[x][status.getStatus()[x].Count - 1]);
                     status.getStatus()[x].RemoveAt(status.getStatus()[x].Count - 1);
 
-                    Console.WriteLine("A:({0}) B:({1}) C:({2})", string.Join(", ", status.getStatus()['A']), string.Join(", ", status.getStatus()['B']), string.Join(", ", status.getStatus()['C']));
+
+                   Console.WriteLine(
+                       "A:{0,-30} B:{1, -30} C:{2, -30}",
+                       "(" + string.Join(", ", status.getStatus()['A']) + ")",
+                       "(" + string.Join(", ", status.getStatus()['B']) + ")",
+                       "(" + string.Join(", ", status.getStatus()['C']) + ")."
+                   );
                 /*zStatus.Add(xStatus[xStatus.Count - 1]);
                 xStatus.RemoveAt(xStatus.Count - 1);
-
-                Console.Write("A=(");
-                foreach (String i in xStatus) {
-                    Console.Write(i + ",");
-                }
-                Console.Write("), B=(");
-                foreach (String i in yStatus)
-                {
-                    Console.Write(i + ",");
-                }
-                Console.Write("), C=(");
-                foreach (String i in zStatus)
-
-                {
-                    Console.Write(i + ",");
-                }
-
-                Console.WriteLine(").");*/
+               */
                 Move(y, x, z, n-1);
             }
         }
